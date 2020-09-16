0ea4aa4da70de9ee8b4542d0db80696c657d81d6
Nicacio Picon <willpicon@hotmail.com>
2020-09-16 17:27:40 -0300

    first commit

---
+++ app/Store.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    protected $fillable = ['name', 'description', 'phone', 'mobile_phone', 'slug'];
+    public function user(){
+        $this->belongsTo(User::class );
+    }
+
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
+}
